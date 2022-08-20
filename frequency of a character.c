af03a766800310413f2a4cbb02ffb6ea69ac2dde
vardhankuppala <101583138+vardhankuppala@users.noreply.github.com>
2022-08-20 20:33:05 +0530

    Add files via upload

---
+++ frequency of a character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char str[1000],ch;
+    int count = 0;
+    fgets(str,sizeof(str),stdin);
+    scanf("%c",&ch);
+    for(int i=0; str[i]!='\0';i++){
+        if (str[i]==ch)
+        count++;
+    }
+    printf("%c=%d",ch,count);
+    return 0;
+}
